94c4228c17163c482244ba7b6151455df6bfeaf2
sourisengupta <sengupta.souri@gmail.com>
2018-10-09 20:46:37 -0400

    Added drop down handling file Code will be added later

---
+++ automationpractice/src/test/java/alertHandling/DropdownHandling.java
@@ -0,0 +1,30 @@
+package alertHandling;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.test.automationpractice.testBase.TestBase;
+
+public class DropdownHandling extends TestBase{
+	String url="";
+	@BeforeTest
+	public void setUp() {
+		init(url);
+		
+	}
+	
+	@Test(priority=1)	
+	
+	@AfterClass
+	public void endTest() {
+		closeApp();
+	}
+	
+}
