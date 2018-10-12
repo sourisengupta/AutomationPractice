8001f703dfa965879bbd88e5b46be7ee3044e458
sourisengupta <sengupta.souri@gmail.com>
2018-10-11 20:30:16 -0400

    Added code for Dropdown handling.

--- automationpractice/src/test/java/dropdownHandling/DropdownHandling.java
+++ automationpractice/src/test/java/dropdownHandling/DropdownHandling.java
@@ -5,6 +5,7 @@ import static org.testng.Assert.assertTrue;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -13,7 +14,7 @@ import org.testng.annotations.Test;
 import com.test.automationpractice.testBase.TestBase;
 
 public class DropdownHandling extends TestBase{
-	String url="";
+	String url="http://the-internet.herokuapp.com/dropdown";
 	@BeforeTest
 	public void setUp() {
 		init(url);
@@ -21,6 +22,12 @@ public class DropdownHandling extends TestBase{
 	}
 	
 	@Test(priority=1)	
+	public void dropdownHandling() {
+		Select select = new Select(driver.findElement(By.cssSelector("#dropdown")));
+		select.selectByIndex(1);
+		select.selectByValue("2");
+		select.selectByVisibleText("Option 1");
+	}
 	
 	@AfterClass
 	public void endTest() {
