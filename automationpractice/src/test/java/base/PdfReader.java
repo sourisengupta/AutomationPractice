b20ab58577deb0492a462c98a85f52f467dbbf40
sourisengupta <sengupta.souri@gmail.com>
2021-09-29 21:45:08 +0530

    Commit

---
+++ automationpractice/src/test/java/base/PdfReader.java
@@ -0,0 +1,12 @@
+package base;
+//import org.apache.pdfbox.pdmodel.PDDocument;
+
+public class PdfReader {
+	public static void main(String[] args) {
+		
+	/*	PDDocument pdfDoc = PDDocument.load(new File("C:\\Users\\souri\\Desktop\\ONGC contract note.pdf"));
+		PDTextStripper stripper = new PDTextStripper();
+		String string = stripper.getText(pdfDoc);*/
+		
+	}
+}
